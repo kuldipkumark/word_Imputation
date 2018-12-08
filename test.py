4e07f43543695b97ade3c34b507c5fe9d9e1e717
Anand Kumar <anandksbiw@gmail.com>
2018-12-08 23:09:12 +0530

    location prediction model added

--- test.py
+++ test.py
@@ -23,4 +23,38 @@ with open('trigram_table.pickle', 'rb') as handle:
 # for i in vocabulary:
 #     print(i+str(vocabulary[i]))
 
+### locating missing word
+while(1):
+        print('input sentence= ')
+        line=input()
+        #fR.write('id,"sentence"\n')
+        cnt = 1
 
+            # Sentence preprocessing
+        words = line.split()
+
+        wordsOriginal = words[:]
+        for k in range(0,len(words)):
+            if words[k] not in vocabulary:
+                words[k] = 'UNKA'
+
+        # Missing word location
+        score = numpy.zeros(len(words)-1)
+        for k in range(1,len(words)):
+            key = words[k-1] + ' ' + words[k] + ' ' + str(0)
+            if key in bigram_table:
+                numNeg = bigram_table[key]
+            else:
+                numNeg = 0
+            key = words[k-1] + ' ' + words[k] + ' ' + str(1)
+            if key in bigram_table:
+                numPos = bigram_table[key]
+            else:
+                numPos = 0
+            if numNeg+numPos!=0:
+                if words[k-1]=='UNKA' or words[k]=='UNKA':
+                    score[k-1] = 1.0*numPos/(numNeg+numPos) - 1.0*numNeg/(numNeg+numPos)
+                else:
+                    score[k-1] = 1.0*numPos**(1+gamma)/(numNeg+numPos) - 1.0*numNeg**(1+gamma)/(numNeg+numPos)
+        location = numpy.argmax(score) + 1
+        print('missing word location= '+str(location))
